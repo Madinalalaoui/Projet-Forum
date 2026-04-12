361c7a1f93ca740e70a8a966eb380bc8934ff1bb
rasheequa <rasheequa.m@gmail.com>
2026-04-12 14:01:16 +0200

    simplification du serveur (wrapper withDB, connexion DB)

--- server/database.js
+++ server/database.js
@@ -6,8 +6,6 @@ export const client = new MongoClient(uri);
 const dbName = "forum";
 
 export async function connectDB() {
-  if (!client.topology || !client.topology.isConnected()) {
-    await client.connect();
-  }
+  await client.connect();
   return client.db(dbName);
 }
